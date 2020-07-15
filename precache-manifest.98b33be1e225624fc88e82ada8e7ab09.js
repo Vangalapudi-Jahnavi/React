38e49c4d6aa5bfb15c7151d467f459b1bbf78a52
jahnavi <jahnavi.vangalapudi@gmail.com>
2020-07-15 22:49:43 +0530

    Updates

---
+++ precache-manifest.98b33be1e225624fc88e82ada8e7ab09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d784e8873db64406001b132eff3ce2ac",
+    "url": "/reactjs/index.html"
+  },
+  {
+    "revision": "d68e59ff2a382cf661be",
+    "url": "/reactjs/static/css/main.80f61ab1.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/reactjs/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactjs/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d68e59ff2a382cf661be",
+    "url": "/reactjs/static/js/main.4b487e85.chunk.js"
+  },
+  {
+    "revision": "6c764a2234c56c8053c8",
+    "url": "/reactjs/static/js/runtime-main.267e7392.js"
+  }
+]);
